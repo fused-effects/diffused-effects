c5099b9fb3a37ab0ceced4250113c2c32002544b
Rob Rix <rob.rix@me.com>
2019-09-11 23:12:05 -0400

    Define a MonadPlus instance for Effects.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -8,6 +8,7 @@ import           Control.Algebra.Class
 import qualified Control.Effect.Choose as Effect
 import qualified Control.Effect.Empty as Effect
 import qualified Control.Effect.Fail as Effect
+import           Control.Monad (MonadPlus(..))
 import qualified Control.Monad.Fail as Fail
 import           Control.Monad.Trans.Class
 
@@ -26,3 +27,5 @@ instance (Algebra sig m, Member Effect.Fail sig) => Fail.MonadFail (Effects m) w
 instance (Algebra sig m, Member Effect.Choose sig, Member Effect.Empty sig) => Alternative.Alternative (Effects m) where
   empty = Effect.empty
   (<|>) = (Effect.<|>)
+
+instance (Algebra sig m, Member Effect.Choose sig, Member Effect.Empty sig) => MonadPlus (Effects m)
