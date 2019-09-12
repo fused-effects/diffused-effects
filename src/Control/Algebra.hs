095fc9784b27b0fc8e2dc698ce0dde4d4603f494
Rob Rix <rob.rix@me.com>
2019-09-11 23:07:24 -0400

    Define a MonadTrans instance for Effects.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -6,9 +6,13 @@ module Control.Algebra
 import           Control.Algebra.Class
 import qualified Control.Effect.Fail as Effect
 import qualified Control.Monad.Fail as Fail
+import           Control.Monad.Trans.Class
 
 newtype Effects m a = Effects { runEffects :: m a }
   deriving (Applicative, Functor, Monad)
+
+instance MonadTrans Effects where
+  lift = Effects
 
 instance (Algebra sig m, Member Effect.Fail sig) => Fail.MonadFail (Effects m) where
   fail = Effects . Effect.fail
