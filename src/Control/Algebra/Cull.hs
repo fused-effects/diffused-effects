7fec1336db55b8e48f5dac1f0f8c95cd897a4af4
Rob Rix <rob.rix@me.com>
2019-09-11 22:44:24 -0400

    :fire: the interface instances for CullC.

--- src/Control/Algebra/Cull.hs
+++ src/Control/Algebra/Cull.hs
@@ -11,17 +11,11 @@ module Control.Algebra.Cull
 , run
 ) where
 
-import Control.Applicative (Alternative(..))
 import Control.Algebra.Class
 import Control.Algebra.NonDet.Church
 import Control.Algebra.Reader
 import Control.Effect.Cull
-import Control.Monad (MonadPlus(..))
-import qualified Control.Monad.Fail as Fail
-import Control.Monad.Fix
-import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
-import Data.Bool (bool)
 
 -- | Run a 'Cull' effect. Branches outside of any 'cull' block will not be pruned.
 --
@@ -30,15 +24,7 @@ runCull :: (m b -> m b -> m b) -> (a -> m b) -> m b -> CullC m a -> m b
 runCull fork leaf nil (CullC m) = runNonDetC (runReader False m) fork leaf nil
 
 newtype CullC m a = CullC { runCullC :: ReaderC Bool (NonDetC m) a }
-  deriving (Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO)
-
-instance (Algebra sig m, Effect sig) => Alternative (CullC m) where
-  empty = send Empty
-  {-# INLINE empty #-}
-  l <|> r = send (Choose (bool r l))
-  {-# INLINE (<|>) #-}
-
-instance (Algebra sig m, Effect sig) => MonadPlus (CullC m)
+  deriving (Applicative, Functor, Monad)
 
 instance MonadTrans CullC where
   lift = CullC . lift . lift
@@ -46,12 +32,12 @@ instance MonadTrans CullC where
 
 instance (Algebra sig m, Effect sig) => Algebra (Cull :+: Empty :+: Choose :+: sig) (CullC m) where
   alg (L (Cull m k))         = CullC (local (const True) (runCullC m)) >>= k
-  alg (R (L Empty))          = CullC empty
+  alg (R (L Empty))          = CullC (send Empty)
   alg (R (R (L (Choose k)))) = CullC $ ReaderC $ \ cull ->
     if cull then
       NonDetC $ \ fork leaf nil ->
         runNonDetC (runReader cull (runCullC (k True))) fork leaf (runNonDetC (runReader cull (runCullC (k False))) fork leaf nil)
     else
-      runReader cull (runCullC (k True)) <|> runReader cull (runCullC (k False))
+      choose (runReader cull (runCullC (k True))) (runReader cull (runCullC (k False)))
   alg (R (R (R other)))      = CullC (alg (R (R (R (handleCoercible other)))))
   {-# INLINE alg #-}
