ad1f2b85e4bb5c87df058946abb45fe0c326cc39
Rob Rix <rob.rix@me.com>
2019-09-11 23:09:43 -0400

    Define an Alternative instance for Effects.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -3,7 +3,10 @@ module Control.Algebra
 ( Effects(..)
 ) where
 
+import qualified Control.Applicative as Alternative
 import           Control.Algebra.Class
+import qualified Control.Effect.Choose as Effect
+import qualified Control.Effect.Empty as Effect
 import qualified Control.Effect.Fail as Effect
 import qualified Control.Monad.Fail as Fail
 import           Control.Monad.Trans.Class
@@ -16,3 +19,7 @@ instance MonadTrans Effects where
 
 instance (Algebra sig m, Member Effect.Fail sig) => Fail.MonadFail (Effects m) where
   fail = lift . Effect.fail
+
+instance (Algebra sig m, Member Effect.Choose sig, Member Effect.Empty sig) => Alternative.Alternative (Effects m) where
+  empty = lift Effect.empty
+  Effects l <|> Effects r = Effects (l Effect.<|> r)
