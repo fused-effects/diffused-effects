9ddafc4420d76880692870c8206fb76eae2ade26
Rob Rix <rob.rix@me.com>
2019-09-11 23:04:37 -0400

    Derive some instances for Effects.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Algebra
 ( Effects(..)
 ) where
 
 newtype Effects m a = Effects { runEffects :: m a }
+  deriving (Applicative, Functor, Monad)
