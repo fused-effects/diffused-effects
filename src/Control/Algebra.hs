87744bd01609175b31eb045fbbe9164bb53c7043
Rob Rix <rob.rix@me.com>
2019-09-11 23:06:53 -0400

    Define a MonadFail instance using the Fail effect.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -1,7 +1,14 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts, GeneralizedNewtypeDeriving, UndecidableInstances #-}
 module Control.Algebra
 ( Effects(..)
 ) where
 
+import           Control.Algebra.Class
+import qualified Control.Effect.Fail as Effect
+import qualified Control.Monad.Fail as Fail
+
 newtype Effects m a = Effects { runEffects :: m a }
   deriving (Applicative, Functor, Monad)
+
+instance (Algebra sig m, Member Effect.Fail sig) => Fail.MonadFail (Effects m) where
+  fail = Effects . Effect.fail
