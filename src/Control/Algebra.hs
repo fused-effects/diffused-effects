b69ff5df737e2020bfb3a7abfae6c0a60e234aa3
Rob Rix <rob.rix@me.com>
2019-09-16 18:35:43 -0400

    Re-export run.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -3,8 +3,10 @@ module Control.Algebra
   module Control.Algebra.Class
 , module Control.Effect.Class
 , module Control.Effect.Sum
+, run
 ) where
 
 import Control.Algebra.Class
+import Control.Algebra.Pure
 import Control.Effect.Class
 import Control.Effect.Sum
