c518188a970cc0f55bf8846613d4bff9b086dbe2
Rob Rix <rob.rix@me.com>
2019-09-16 18:21:40 -0400

    Re-export the Algebra.Class & Effect.Class modules.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -1,2 +1,8 @@
 module Control.Algebra
-() where
+( -- * Re-exports
+  module Control.Algebra.Class
+, module Control.Effect.Class
+) where
+
+import Control.Algebra.Class
+import Control.Effect.Class
