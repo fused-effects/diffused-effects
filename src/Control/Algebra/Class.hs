290c4a8bc239809c96e913afb61366010de30ee3
Rob Rix <rob.rix@me.com>
2019-09-16 18:42:18 -0400

    Move the Algebra instance for PureC into Control.Algebra.Pure.

--- src/Control/Algebra/Class.hs
+++ src/Control/Algebra/Class.hs
@@ -3,15 +3,9 @@ module Control.Algebra.Class
 ( Algebra(..)
 ) where
 
-import qualified Control.Algebra.Pure as Pure
-import           Control.Effect.Class
+import Control.Effect.Class
 
 -- | The class of algebras (effect handlers) for carriers (monad transformers) over signatures (effects), whose actions are given by the 'alg' method.
 class (HFunctor sig, Monad m) => Algebra sig m | m -> sig where
   -- | Construct a value in the carrier for an effect signature (typically a sum of a handled effect and any remaining effects).
   alg :: sig m a -> m a
-
-
-instance Algebra Pure.Pure Pure.PureC where
-  alg v = case v of {}
-  {-# INLINE alg #-}
