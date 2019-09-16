57d2948719168d1fb9f600cb8e1a4afac08419d2
Rob Rix <rob.rix@me.com>
2019-09-16 18:24:38 -0400

    Re-export Control.Effect.Sum.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -2,7 +2,9 @@ module Control.Algebra
 ( -- * Re-exports
   module Control.Algebra.Class
 , module Control.Effect.Class
+, module Control.Effect.Sum
 ) where
 
 import Control.Algebra.Class
 import Control.Effect.Class
+import Control.Effect.Sum
