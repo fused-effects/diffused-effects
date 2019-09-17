7242c7d1bb046e213814420fa732ce3ed0d28445
Rob Rix <rob.rix@me.com>
2019-09-16 20:57:43 -0400

    Derive some instances.

--- src/Control/Algebra/Named.hs
+++ src/Control/Algebra/Named.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, PolyKinds #-}
 module Control.Algebra.Named
 ( NamedC(..)
 ) where
 
 newtype NamedC (name :: k) m a = NamedC { runNamed :: m a }
+  deriving (Applicative, Functor, Monad)
