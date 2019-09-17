bd4598c10e094968872e41d5afc4174ccc822698
Rob Rix <rob.rix@me.com>
2019-09-16 21:13:18 -0400

    Define an Algebra instance for NamedC.

--- src/Control/Algebra/Named.hs
+++ src/Control/Algebra/Named.hs
@@ -1,7 +1,13 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, PolyKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, MultiParamTypeClasses, PolyKinds, UndecidableInstances #-}
 module Control.Algebra.Named
 ( NamedC(..)
 ) where
 
+import Control.Algebra
+import Control.Effect.Sum.Named
+
 newtype NamedC (name :: k) m a = NamedC { runNamed :: m a }
   deriving (Applicative, Functor, Monad)
+
+instance Algebra sig m => Algebra (Named name sig) (NamedC name m) where
+  alg = NamedC . alg . handleCoercible . getNamed
