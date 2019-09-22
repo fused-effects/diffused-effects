4848944bd7a8495baed9449523b7974fded6a1fa
Rob Rix <rob.rix@me.com>
2019-09-22 14:45:12 -0400

    Define a named counterpart to Has.

--- src/Control/Effect/State/Named.hs
+++ src/Control/Effect/State/Named.hs
@@ -15,14 +15,14 @@ import Control.Effect.State (State(..))
 -- | Get the current state value.
 --
 --   prop> snd (run (runState a get)) === a
-get :: forall name s m . (NamedMember name (State s) (Signature m), Algebra m) => m s
+get :: forall name s m . HasNamed name (State s) m => m s
 get = sendNamed @name (Get pure)
 {-# INLINEABLE get #-}
 
 -- | Project a function out of the current state value.
 --
 --   prop> snd (run (runState a (gets (applyFun f)))) === applyFun f a
-gets :: forall name s m a . (NamedMember name (State s) (Signature m), Algebra m) => (s -> a) -> m a
+gets :: forall name s m a . HasNamed name (State s) m => (s -> a) -> m a
 gets f = sendNamed @name (Get (pure . f))
 {-# INLINEABLE gets #-}
 
@@ -31,7 +31,7 @@ gets f = sendNamed @name (Get (pure . f))
 --   prop> fst (run (runState a (put b))) === b
 --   prop> snd (run (runState a (get <* put b))) === a
 --   prop> snd (run (runState a (put b *> get))) === b
-put :: forall name s m . (NamedMember name (State s) (Signature m), Algebra m) => s -> m ()
+put :: forall name s m . HasNamed name (State s) m => s -> m ()
 put s = sendNamed @name (Put s (pure ()))
 {-# INLINEABLE put #-}
 
@@ -39,7 +39,7 @@ put s = sendNamed @name (Put s (pure ()))
 --   This is strict in the new state.
 --
 --   prop> fst (run (runState a (modify (+1)))) === (1 + a :: Integer)
-modify :: forall name s m . (NamedMember name (State s) (Signature m), Algebra m) => (s -> s) -> m ()
+modify :: forall name s m . HasNamed name (State s) m => (s -> s) -> m ()
 modify f = do
   a <- get @name
   put @name $! f a
@@ -47,6 +47,6 @@ modify f = do
 
 -- | Replace the state value with the result of applying a function to the current state value.
 --   This is lazy in the new state; injudicious use of this function may lead to space leaks.
-modifyLazy :: forall name s m . (NamedMember name (State s) (Signature m), Algebra m) => (s -> s) -> m ()
+modifyLazy :: forall name s m . HasNamed name (State s) m => (s -> s) -> m ()
 modifyLazy f = get @name >>= put @name . f
 {-# INLINEABLE modifyLazy #-}
