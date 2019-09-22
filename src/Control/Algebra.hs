0d405a6ce9a60551cbb65883ef0e2533860ee58f
Rob Rix <rob.rix@me.com>
2019-09-22 13:37:13 -0400

    Errors for Has constraints talk about HasIn instead of Member.

--- src/Control/Algebra.hs
+++ src/Control/Algebra.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ConstraintKinds, FlexibleContexts #-}
+{-# LANGUAGE ConstraintKinds, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, TypeOperators #-}
 module Control.Algebra
 ( -- * Re-exports
   module Control.Algebra.Class
@@ -14,9 +14,14 @@ import Control.Algebra.Pure
 import Control.Effect.Class
 import Control.Effect.Sum
 
-type Has eff m = (Member eff (Signature m), Algebra m)
+type Has eff m = HasIn (Signature m) eff m
 
 -- | Construct a request for an effect to be interpreted by some handler later on.
 send :: Has eff m => eff m a -> m a
 send = alg . inj
 {-# INLINE send #-}
+
+
+class (Algebra m, Member eff sig) => HasIn sig eff m
+instance {-# OVERLAPPABLE #-} (Algebra m, Member eff (l :+: r)) => HasIn (l :+: r) eff m
+instance {-# OVERLAPPABLE #-} Algebra m => HasIn eff eff m
