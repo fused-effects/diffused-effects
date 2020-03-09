c074d5c61fede2bfabc0e54fed86903d62413f7d
Rob Rix <rob.rix@me.com>
2020-03-09 00:56:04 -0400

    Define an Algebra instance for Either e.

--- src/Control/Algebra/Class.hs
+++ src/Control/Algebra/Class.hs
@@ -1,12 +1,25 @@
-{-# LANGUAGE FlexibleContexts, TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeFamilies #-}
 module Control.Algebra.Class
 ( Algebra(..)
 ) where
 
+import Control.Effect.Catch.Internal
 import Control.Effect.Class
+import Control.Effect.Error.Internal
+import Control.Effect.Sum
+import Control.Effect.Throw.Internal
 
 -- | The class of algebras (effect handlers) for carriers (monad transformers) over signatures (effects), whose actions are given by the 'alg' method.
 class (HFunctor (Signature m), Monad m) => Algebra m where
   type Signature m :: (* -> *) -> (* -> *)
   -- | Construct a value in the carrier for an effect signature (typically a sum of a handled effect and any remaining effects).
   alg :: Signature m m a -> m a
+
+instance Algebra (Either e) where
+  type Signature (Either e) = Error e
+
+  alg = \case
+    L (Throw e)     -> Left e
+    R (Catch m h k) -> either h pure m >>= k
