d61f2d2100ea68b13ee252c679a0bccf8a616256
Rob Rix <rob.rix@me.com>
2020-03-09 00:40:42 -0400

    :fire: MonadUnliftIO instances.

--- src/Control/Algebra/Lift.hs
+++ src/Control/Algebra/Lift.hs
@@ -17,7 +17,6 @@ import Control.Monad (MonadPlus(..))
 import qualified Control.Monad.Fail as Fail
 import Control.Monad.Fix
 import Control.Monad.IO.Class
-import Control.Monad.IO.Unlift
 import Control.Monad.Trans.Class
 
 -- | Extract a 'Lift'ed 'Monad'ic action from an effectful computation.
@@ -33,9 +32,3 @@ instance MonadTrans LiftC where
 instance Monad m => Algebra (LiftC m) where
   type Signature (LiftC m) = Lift m
   alg = LiftC . (>>= runLiftC) . unLift
-
-instance MonadUnliftIO m => MonadUnliftIO (LiftC m) where
-  askUnliftIO = LiftC $ withUnliftIO $ \u -> return (UnliftIO (unliftIO u . runLiftC))
-  {-# INLINE askUnliftIO #-}
-  withRunInIO inner = LiftC $ withRunInIO $ \run -> inner (run . runLiftC)
-  {-# INLINE withRunInIO #-}
