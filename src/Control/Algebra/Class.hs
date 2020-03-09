ee8190f2d10efcdb6c6f4e94cc72f0fd6ea34b7d
Rob Rix <rob.rix@me.com>
2020-03-09 01:01:25 -0400

    Move the Algebra instance for Maybe down.

--- src/Control/Algebra/Class.hs
+++ src/Control/Algebra/Class.hs
@@ -12,15 +12,15 @@ import Control.Effect.Error.Internal
 import Control.Effect.Sum
 import Control.Effect.Throw.Internal
 
-instance Algebra Maybe where
-  type Signature Maybe = Empty
-
-  alg Empty = Nothing
-
 class (HFunctor (Signature m), Monad m) => Algebra m where
   type Signature m :: (* -> *) -> (* -> *)
 
   alg :: Signature m m a -> m a
+
+instance Algebra Maybe where
+  type Signature Maybe = Empty
+
+  alg Empty = Nothing
 
 instance Algebra (Either e) where
   type Signature (Either e) = Error e
