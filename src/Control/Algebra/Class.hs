f75175c9aaef32ee2ec827619e3947842a986644
Rob Rix <rob.rix@me.com>
2020-03-09 01:01:13 -0400

    :fire: some doc comments.

--- src/Control/Algebra/Class.hs
+++ src/Control/Algebra/Class.hs
@@ -17,10 +17,9 @@ instance Algebra Maybe where
 
   alg Empty = Nothing
 
--- | The class of algebras (effect handlers) for carriers (monad transformers) over signatures (effects), whose actions are given by the 'alg' method.
 class (HFunctor (Signature m), Monad m) => Algebra m where
   type Signature m :: (* -> *) -> (* -> *)
-  -- | Construct a value in the carrier for an effect signature (typically a sum of a handled effect and any remaining effects).
+
   alg :: Signature m m a -> m a
 
 instance Algebra (Either e) where
