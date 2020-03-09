dfa3d5bbaac42fc4aa49751572084f832a02a410
Rob Rix <rob.rix@me.com>
2020-03-09 02:10:16 -0400

    Fix the returning Trace carrier.

--- src/Algebra/Trace/Returning.hs
+++ src/Algebra/Trace/Returning.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, TypeFamilies, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Algebra.Trace.Returning
 ( -- * Trace effect
   module Effect.Trace
@@ -10,27 +14,27 @@ module Algebra.Trace.Returning
 , run
 ) where
 
-import Algebra
-import Algebra.State.Strict
-import Control.Applicative (Alternative(..))
-import Effect.Trace
-import Control.Monad (MonadPlus(..))
+import           Algebra
+import           Control.Applicative (Alternative(..))
+import           Control.Monad (MonadPlus(..))
 import qualified Control.Monad.Fail as Fail
-import Control.Monad.Fix
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Class
-import Data.Bifunctor (first)
+import           Control.Monad.Fix
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.State.Strict
+import           Effect.Trace
 
 -- | Run a 'Trace' effect, returning all traces as a list.
 --
 --   prop> run (runTrace (trace a *> trace b *> pure c)) === ([a, b], c)
 runTrace :: Functor m => TraceC m a -> m ([String], a)
-runTrace = fmap (first reverse) . runState [] . runTraceC
+runTrace = fmap (\ (a, s) -> (reverse s, a)) . (`runStateT` []) . runTraceC
 
-newtype TraceC m a = TraceC { runTraceC :: StateC [String] m a }
+newtype TraceC m a = TraceC { runTraceC :: StateT [String] m a }
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus, MonadTrans)
 
 instance (Algebra m, Effect (Sig m)) => Algebra (TraceC m) where
   type Sig (TraceC m) = Trace :+: Sig m
+
   alg (L (Trace m k)) = TraceC (modify (m :)) *> k
   alg (R other)       = TraceC (alg (R (handleCoercible other)))
