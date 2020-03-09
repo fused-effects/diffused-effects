bd77cea3dc8f1b08f3d2e123ed5470116000f350
Rob Rix <rob.rix@me.com>
2020-03-09 09:40:21 -0400

    Rename CutC to CutT.

--- src/Algebra/Cut/Church.hs
+++ src/Algebra/Cut/Church.hs
@@ -12,7 +12,7 @@ module Algebra.Cut.Church
   runCut
 , runCutA
 , runCutM
-, CutC(..)
+, CutT(..)
   -- * Cut effect
 , module Effect.Cut
   -- * NonDet effects
@@ -29,53 +29,53 @@ import           Data.Functor.Identity
 import           Effect.Cut
 import           Effect.NonDet
 
-runCut :: (a -> m b -> m b) -> m b -> m b -> CutC m a -> m b
-runCut cons nil fail (CutC runCutC) = runCutC cons nil fail
+runCut :: (a -> m b -> m b) -> m b -> m b -> CutT m a -> m b
+runCut cons nil fail (CutT runCutT) = runCutT cons nil fail
 
-runCutA :: (Alt.Alternative f, Applicative m) => CutC m a -> m (f a)
+runCutA :: (Alt.Alternative f, Applicative m) => CutT m a -> m (f a)
 runCutA = runCut (fmap . (Alt.<|>) . pure) (pure Alt.empty) (pure Alt.empty)
 
-runCutM :: (Applicative m, Monoid b) => (a -> b) -> CutC m a -> m b
+runCutM :: (Applicative m, Monoid b) => (a -> b) -> CutT m a -> m b
 runCutM leaf = runCut (fmap . mappend . leaf) (pure mempty) (pure mempty)
 
-newtype CutC m a = CutC (forall b . (a -> m b -> m b) -> m b -> m b -> m b)
+newtype CutT m a = CutT (forall b . (a -> m b -> m b) -> m b -> m b -> m b)
   deriving (Functor)
 
-instance Applicative (CutC m) where
-  pure a = CutC (\ cons nil _ -> cons a nil)
+instance Applicative (CutT m) where
+  pure a = CutT (\ cons nil _ -> cons a nil)
   {-# INLINE pure #-}
-  CutC f <*> CutC a = CutC $ \ cons nil fail ->
+  CutT f <*> CutT a = CutT $ \ cons nil fail ->
     f (\ f' fs -> a (cons . f') fs fail) nil fail
   {-# INLINE (<*>) #-}
 
-instance Monad (CutC m) where
-  CutC a >>= f = CutC $ \ cons nil fail ->
+instance Monad (CutT m) where
+  CutT a >>= f = CutT $ \ cons nil fail ->
     a (\ a' as -> runCut cons as fail (f a')) nil fail
   {-# INLINE (>>=) #-}
 
 -- | A single fixpoint is shared between all branches.
-instance MonadFix m => MonadFix (CutC m) where
-  mfix f = CutC $ \ cons nil fail -> mfix
+instance MonadFix m => MonadFix (CutT m) where
+  mfix f = CutT $ \ cons nil fail -> mfix
     (toCut . f . run . fromCut)
     >>= run . runCut (fmap . cons) (pure nil) (pure fail) where
     toCut = runCut (fmap . (<|>) . pure) (pure empty) (pure cutfail)
-    fromCut = runCut (<$) (error "mfix CutC: empty") (error "mfix CutC: cutfail")
+    fromCut = runCut (<$) (error "mfix CutT: empty") (error "mfix CutT: cutfail")
   {-# INLINE mfix #-}
 
-instance MonadTrans CutC where
-  lift m = CutC (\ cons nil _ -> m >>= flip cons nil)
+instance MonadTrans CutT where
+  lift m = CutT (\ cons nil _ -> m >>= flip cons nil)
   {-# INLINE lift #-}
 
-instance Algebra m => Algebra (CutC m) where
-  type Sig (CutC m) = Cut :+: NonDet :+: Sig m
+instance Algebra m => Algebra (CutT m) where
+  type Sig (CutT m) = Cut :+: NonDet :+: Sig m
 
-  alg (ctx :: ctx ()) (hdl :: forall x . ctx (n x) -> CutC m (ctx x)) = \case
-    L Cutfail    -> CutC $ \ _    _   fail -> fail
-    L (Call m k) -> CutC $ \ cons nil fail -> runCut (\ a as -> runCut cons as fail (hdl (fmap k a))) nil nil (hdl (m <$ ctx))
-    R (L (L Empty))      -> CutC $ \ _ nil _ -> nil
-    R (L (R (Choose k))) -> CutC $ \ cons nil fail -> runCut cons (runCut cons nil fail (hdl (k False <$ ctx))) fail (hdl (k True <$ ctx))
-    R (R other)          -> CutC $ \ cons nil fail -> thread (pure ctx) dst other >>= runIdentity . runCut (coerce cons) (coerce nil) (coerce fail)
+  alg (ctx :: ctx ()) (hdl :: forall x . ctx (n x) -> CutT m (ctx x)) = \case
+    L Cutfail    -> CutT $ \ _    _   fail -> fail
+    L (Call m k) -> CutT $ \ cons nil fail -> runCut (\ a as -> runCut cons as fail (hdl (fmap k a))) nil nil (hdl (m <$ ctx))
+    R (L (L Empty))      -> CutT $ \ _ nil _ -> nil
+    R (L (R (Choose k))) -> CutT $ \ cons nil fail -> runCut cons (runCut cons nil fail (hdl (k False <$ ctx))) fail (hdl (k True <$ ctx))
+    R (R other)          -> CutT $ \ cons nil fail -> thread (pure ctx) dst other >>= runIdentity . runCut (coerce cons) (coerce nil) (coerce fail)
     where
-    dst :: CutC Identity (ctx (n a)) -> m (CutC Identity (ctx a))
+    dst :: CutT Identity (ctx (n a)) -> m (CutT Identity (ctx a))
     dst = runIdentity . runCut (fmap . liftA2 (<|>) . runCut (fmap . (<|>) . pure) (pure empty) (pure cutfail) . hdl) (pure (pure empty)) (pure (pure cutfail))
   {-# INLINE alg #-}
