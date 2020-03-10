50dc340d9414792ca8c0be69752bb3be321f60ef
Rob Rix <rob.rix@me.com>
2020-03-10 18:01:57 -0400

    Combine contexts.

--- src/Algebra/Trans.hs
+++ src/Algebra/Trans.hs
@@ -79,7 +79,7 @@ instance AlgebraTrans t m => Algebra (AlgT t m) where
 
   alg = mapLowerTDist AlgT (Hom runAlgT ~<) . algDefault
 
-algDefault :: AlgebraTrans t m => Functor ctx => (SigT t :+: Sig m) n a -> LowerT ctx n (t m) (ctx a)
+algDefault :: (AlgebraTrans t m, Functor ctx) => (SigT t :+: Sig m) n a -> LowerT ctx n (t m) (ctx a)
 algDefault = \case
   L l -> algT l
   R r -> LowerT $ \ ctx1 hdl1 -> liftWith $ LowerT $ \ ctx2 hdl2 -> getCompose <$> runLowerT (Compose (ctx1 <$ ctx2)) (hdl2 <~< hdl1) (alg r)
