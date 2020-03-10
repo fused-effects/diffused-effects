fb1a1d40426b726c9beb01ae27a157af8cce5225
Rob Rix <rob.rix@me.com>
2020-03-10 16:46:38 -0400

    Define precomposition with a homomorphism.

--- src/Algebra/Trans.hs
+++ src/Algebra/Trans.hs
@@ -19,6 +19,7 @@ module Algebra.Trans
 , runDist
 , homDist
 , (<~<)
+, (~<)
 , Dist(..)
 , runLowerT
 , runLowerTHom
@@ -93,6 +94,9 @@ homDist hom = Dist (fmap Identity . runHom hom . runIdentity)
 
 (<~<) :: (Functor n, Functor ctx2) => Dist ctx1 l m -> Dist ctx2 m n -> Dist (Compose ctx2 ctx1) l n
 Dist hdl1 <~< Dist hdl2 = Dist (fmap Compose . hdl2 . fmap hdl1 . getCompose)
+
+(~<) :: Functor ctx => Hom l m -> Dist ctx m n -> Dist ctx l n
+Hom hdl1 ~< Dist hdl2 = Dist (hdl2 . fmap hdl1)
 
 newtype Dist ctx m n = Dist (forall x . ctx (m x) -> n (ctx x))
 
