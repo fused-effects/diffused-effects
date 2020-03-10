d605ee8a754d7e7a9d231fca3cf6e33a7026f8c2
Rob Rix <rob.rix@me.com>
2020-03-10 17:53:29 -0400

    Give fixity & precedence for the Dist composition operators.

--- src/Algebra/Trans.hs
+++ src/Algebra/Trans.hs
@@ -101,20 +101,32 @@ homDist hom = Dist (fmap Identity . appHom hom . runIdentity)
 (>~>) :: (Functor n, Functor ctx2) => Dist ctx1 l m -> Dist ctx2 m n -> Dist (Compose ctx2 ctx1) l n
 Dist hdl1 >~> Dist hdl2 = Dist (fmap Compose . hdl2 . fmap hdl1 . getCompose)
 
+infixr 1 >~>
+
 (<~<) :: (Functor n, Functor ctx2) => Dist ctx2 m n -> Dist ctx1 l m -> Dist (Compose ctx2 ctx1) l n
 Dist hdl1 <~< Dist hdl2 = Dist (fmap Compose . hdl1 . fmap hdl2 . getCompose)
+
+infixr 1 <~<
 
 (~>) :: Functor ctx => Hom l m -> Dist ctx m n -> Dist ctx l n
 Hom hdl1 ~> Dist hdl2 = Dist (hdl2 . fmap hdl1)
 
+infixr 1 ~>
+
 (<~) :: Functor ctx => Dist ctx m n -> Hom l m -> Dist ctx l n
 Dist hdl1 <~ Hom hdl2 = Dist (hdl1 . fmap hdl2)
+
+infixr 1 <~
 
 (>~) :: Dist ctx l m -> Hom m n -> Dist ctx l n
 Dist hdl1 >~ Hom hdl2 = Dist (hdl2 . hdl1)
 
+infixr 1 >~
+
 (~<) :: Hom m n -> Dist ctx l m -> Dist ctx l n
 Hom hdl1 ~< Dist hdl2 = Dist (hdl1 . hdl2)
+
+infixr 1 ~<
 
 newtype Dist ctx m n = Dist { appDist :: forall x . ctx (m x) -> n (ctx x) }
 
