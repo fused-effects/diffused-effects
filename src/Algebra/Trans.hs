e296b6b979ec1b13b34d8d99f5dc5ae5bbd85a31
Rob Rix <rob.rix@me.com>
2020-03-09 23:45:51 -0400

    Give algT the context and distributive law again.

--- src/Algebra/Trans.hs
+++ src/Algebra/Trans.hs
@@ -25,7 +25,7 @@ import           Effect.Throw.Internal
 class MonadTrans t => AlgebraTrans t where
   type SigT t :: (Type -> Type) -> (Type -> Type)
 
-  algT :: Monad m => SigT t (t m) a -> t m a
+  algT :: (Monad m, Functor ctx) => ctx () -> (forall a . ctx (n a) -> t m (ctx a)) -> SigT t n a -> t m (ctx a)
 
   liftWith :: Monad m => (forall ctx . Functor ctx => ctx () -> (forall a . ctx (t m a) -> m (ctx a)) -> m (ctx a)) -> t m a
 
@@ -33,35 +33,35 @@ class MonadTrans t => AlgebraTrans t where
 instance AlgebraTrans (R.ReaderT r) where
   type SigT (R.ReaderT r) = Reader r
 
-  algT = \case
-    Ask       k -> R.ask       >>= k
-    Local f m k -> R.local f m >>= k
+  algT ctx hdl = \case
+    Ask       k -> R.ask                      >>= hdl . (<$ ctx) . k
+    Local f m k -> R.local f (hdl (m <$ ctx)) >>= hdl . fmap k
 
   liftWith f = R.ReaderT $ \ r -> runIdentity <$> f (Identity ()) (fmap Identity . (`R.runReaderT` r) . runIdentity)
 
 instance AlgebraTrans (E.ExceptT e) where
   type SigT (E.ExceptT e) = Error e
 
-  algT = \case
+  algT ctx hdl = \case
     L (Throw e)     -> E.throwE e
-    R (Catch m h k) -> E.catchE m h >>= k
+    R (Catch m h k) -> E.catchE (hdl (m <$ ctx)) (hdl . (<$ ctx) . h) >>= hdl . fmap k
 
   liftWith f = E.ExceptT $ f (Right ()) (either (pure . Left) E.runExceptT)
 
 instance AlgebraTrans (S.L.StateT s) where
   type SigT (S.L.StateT s) = State s
 
-  algT = \case
-    Get   k -> S.L.get   >>= k
-    Put s k -> S.L.put s >>  k
+  algT ctx hdl = \case
+    Get   k -> S.L.get   >>= hdl . (<$ ctx) . k
+    Put s k -> S.L.put s >>  hdl (k <$ ctx)
 
   liftWith f = S.L.StateT $ \ s -> swap <$> f (s, ()) (fmap swap . uncurry (flip S.L.runStateT))
 
 instance AlgebraTrans (S.S.StateT s) where
   type SigT (S.S.StateT s) = State s
 
-  algT = \case
-    Get   k -> S.S.get   >>= k
-    Put s k -> S.S.put s >>  k
+  algT ctx hdl = \case
+    Get   k -> S.S.get   >>= hdl . (<$ ctx) . k
+    Put s k -> S.S.put s >>  hdl (k <$ ctx)
 
   liftWith f = S.S.StateT $ \ s -> swap <$> f (s, ()) (fmap swap . uncurry (flip S.S.runStateT))
