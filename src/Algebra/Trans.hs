4f171c375e04e5dfcc1eddceff1c5ea6b9dbde15
Rob Rix <rob.rix@me.com>
2020-03-09 22:39:03 -0400

    Define an AlgebraTrans instance for ExceptT.

--- src/Algebra/Trans.hs
+++ src/Algebra/Trans.hs
@@ -6,8 +6,13 @@ module Algebra.Trans
 ) where
 
 import           Control.Monad.Trans.Class
+import qualified Control.Monad.Trans.Except as E
 import qualified Control.Monad.Trans.Reader as R
+import           Effect.Catch.Internal
+import           Effect.Error.Internal
 import           Effect.Reader.Internal
+import           Effect.Sum
+import           Effect.Throw.Internal
 
 class MonadTrans t => AlgebraTrans t where
   type SigT t :: (* -> *) -> (* -> *)
@@ -20,3 +25,10 @@ instance AlgebraTrans (R.ReaderT r) where
   algT = \case
     Ask       k -> R.ask       >>= k
     Local f m k -> R.local f m >>= k
+
+instance AlgebraTrans (E.ExceptT e) where
+  type SigT (E.ExceptT e) = Error e
+
+  algT = \case
+    L (Throw e)     -> E.throwE e
+    R (Catch m h k) -> E.catchE m h >>= k
