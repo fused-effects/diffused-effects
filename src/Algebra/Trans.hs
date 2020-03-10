00981e9a856746e48025f0e66111cb590ebd25b5
Rob Rix <rob.rix@me.com>
2020-03-09 22:35:28 -0400

    Define an AlgebraTrans instance for ReaderT.

--- src/Algebra/Trans.hs
+++ src/Algebra/Trans.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 module Algebra.Trans
@@ -6,8 +7,17 @@ module Algebra.Trans
 
 import qualified Algebra as A
 import           Control.Monad.Trans.Class
+import qualified Control.Monad.Trans.Reader as R
+import           Effect.Reader.Internal
 
 class MonadTrans t => AlgebraTrans t where
   type SigT t :: (* -> *) -> (* -> *)
 
   algT :: A.Algebra m => SigT t (t m) a -> t m a
+
+instance AlgebraTrans (R.ReaderT r) where
+  type SigT (R.ReaderT r) = Reader r
+
+  algT = \case
+    Ask       k -> R.ask       >>= k
+    Local f m k -> R.local f m >>= k
