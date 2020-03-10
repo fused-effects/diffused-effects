f7cd665164c92d7752492e3a2caeef046658934f
Rob Rix <rob.rix@me.com>
2020-03-10 00:04:50 -0400

    Split liftWith out into a new typeclass.

--- src/Algebra/Trans.hs
+++ src/Algebra/Trans.hs
@@ -36,13 +36,13 @@ class Monad m => Algebra m where
 
   alg :: Functor ctx => ctx () -> (forall x . ctx (n x) -> m (ctx x)) -> Sig m n a -> m (ctx a)
 
+class MonadTrans t => MonadLift t where
+  liftWith :: Monad m => (forall ctx . Functor ctx => ctx () -> (forall a . ctx (t m a) -> m (ctx a)) -> m (ctx a)) -> t m a
 
-class (MonadTrans t, Algebra m, Monad (t m)) => AlgebraTrans t m where
+class (MonadLift t, Algebra m, Monad (t m)) => AlgebraTrans t m where
   type SigT t :: (Type -> Type) -> (Type -> Type)
 
   algT :: Functor ctx => ctx () -> (forall a . ctx (n a) -> t m (ctx a)) -> SigT t n a -> t m (ctx a)
-
-  liftWith :: (forall ctx . Functor ctx => ctx () -> (forall a . ctx (t m a) -> m (ctx a)) -> m (ctx a)) -> t m a
 
 
 newtype AlgT t (m :: Type -> Type) a = AlgT { runAlgT :: t m a }
@@ -59,6 +59,9 @@ algDefault ctx1 hdl1 = \case
   R r -> liftWith $ \ ctx2 hdl2 -> getCompose <$> alg (Compose (ctx1 <$ ctx2)) (fmap Compose . hdl2 . fmap hdl1 . getCompose) r
 
 
+instance MonadLift (R.ReaderT r) where
+  liftWith f = R.ReaderT $ \ r -> runIdentity <$> f (Identity ()) (fmap Identity . (`R.runReaderT` r) . runIdentity)
+
 instance Algebra m => AlgebraTrans (R.ReaderT r) m where
   type SigT (R.ReaderT r) = Reader r
 
@@ -66,9 +69,10 @@ instance Algebra m => AlgebraTrans (R.ReaderT r) m where
     Ask       k -> R.ask                      >>= hdl . (<$ ctx) . k
     Local f m k -> R.local f (hdl (m <$ ctx)) >>= hdl . fmap k
 
-  liftWith f = R.ReaderT $ \ r -> runIdentity <$> f (Identity ()) (fmap Identity . (`R.runReaderT` r) . runIdentity)
-
 deriving via AlgT (R.ReaderT r) m instance Algebra m => Algebra (R.ReaderT r m)
+
+instance MonadLift (E.ExceptT e) where
+  liftWith f = E.ExceptT $ f (Right ()) (either (pure . Left) E.runExceptT)
 
 instance Algebra m => AlgebraTrans (E.ExceptT e) m where
   type SigT (E.ExceptT e) = Error e
@@ -77,9 +81,10 @@ instance Algebra m => AlgebraTrans (E.ExceptT e) m where
     L (Throw e)     -> E.throwE e
     R (Catch m h k) -> E.catchE (hdl (m <$ ctx)) (hdl . (<$ ctx) . h) >>= hdl . fmap k
 
-  liftWith f = E.ExceptT $ f (Right ()) (either (pure . Left) E.runExceptT)
-
 deriving via AlgT (E.ExceptT e) m instance Algebra m => Algebra (E.ExceptT e m)
+
+instance MonadLift (S.L.StateT s) where
+  liftWith f = S.L.StateT $ \ s -> swap <$> f (s, ()) (fmap swap . uncurry (flip S.L.runStateT))
 
 instance Algebra m => AlgebraTrans (S.L.StateT s) m where
   type SigT (S.L.StateT s) = State s
@@ -88,9 +93,10 @@ instance Algebra m => AlgebraTrans (S.L.StateT s) m where
     Get   k -> S.L.get   >>= hdl . (<$ ctx) . k
     Put s k -> S.L.put s >>  hdl (k <$ ctx)
 
-  liftWith f = S.L.StateT $ \ s -> swap <$> f (s, ()) (fmap swap . uncurry (flip S.L.runStateT))
-
 deriving via AlgT (S.L.StateT s) m instance Algebra m => Algebra (S.L.StateT s m)
+
+instance MonadLift (S.S.StateT s) where
+  liftWith f = S.S.StateT $ \ s -> swap <$> f (s, ()) (fmap swap . uncurry (flip S.S.runStateT))
 
 instance Algebra m => AlgebraTrans (S.S.StateT s) m where
   type SigT (S.S.StateT s) = State s
@@ -98,7 +104,5 @@ instance Algebra m => AlgebraTrans (S.S.StateT s) m where
   algT ctx hdl = \case
     Get   k -> S.S.get   >>= hdl . (<$ ctx) . k
     Put s k -> S.S.put s >>  hdl (k <$ ctx)
-
-  liftWith f = S.S.StateT $ \ s -> swap <$> f (s, ()) (fmap swap . uncurry (flip S.S.runStateT))
 
 deriving via AlgT (S.S.StateT s) m instance Algebra m => Algebra (S.S.StateT s m)
