6bc667141870cfa6dcf8357045c1e2c15755b3b2
Rob Rix <rob.rix@me.com>
2020-03-10 09:39:30 -0400

    Define an algebra monad.

--- src/Algebra/Trans.hs
+++ src/Algebra/Trans.hs
@@ -72,6 +72,10 @@ runDist cm (Dist run) = run cm
 newtype Dist ctx m n = Dist (forall x . ctx (m x) -> n (ctx x))
 
 
+newtype AlgM ctx m n a = AlgM { runAlgM :: R.ReaderT (ctx ()) (R.ReaderT (Dist ctx m n) n) a }
+  deriving (Applicative, Functor, Monad)
+
+
 instance MonadLift (R.ReaderT r) where
   liftWith f = R.ReaderT $ \ r -> runIdentity <$> f (Identity ()) (fmap Identity . (`R.runReaderT` r) . runIdentity)
 
