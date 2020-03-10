caf1bc137ebc04e7543a541ae5c8b37c80b40b59
Rob Rix <rob.rix@me.com>
2020-03-09 22:37:18 -0400

    Weaken the constraint on m to Monad.

--- src/Algebra/Trans.hs
+++ src/Algebra/Trans.hs
@@ -5,7 +5,6 @@ module Algebra.Trans
 ( AlgebraTrans(..)
 ) where
 
-import qualified Algebra as A
 import           Control.Monad.Trans.Class
 import qualified Control.Monad.Trans.Reader as R
 import           Effect.Reader.Internal
@@ -13,7 +12,7 @@ import           Effect.Reader.Internal
 class MonadTrans t => AlgebraTrans t where
   type SigT t :: (* -> *) -> (* -> *)
 
-  algT :: A.Algebra m => SigT t (t m) a -> t m a
+  algT :: Monad m => SigT t (t m) a -> t m a
 
 instance AlgebraTrans (R.ReaderT r) where
   type SigT (R.ReaderT r) = Reader r
