1da00a45ef96a2cb1ccba70f5e288f16bac876e3
Rob Rix <rob.rix@me.com>
2020-03-09 22:39:38 -0400

    Write Type out in the kind signature for SigT.

--- src/Algebra/Trans.hs
+++ src/Algebra/Trans.hs
@@ -8,6 +8,7 @@ module Algebra.Trans
 import           Control.Monad.Trans.Class
 import qualified Control.Monad.Trans.Except as E
 import qualified Control.Monad.Trans.Reader as R
+import           Data.Kind (Type)
 import           Effect.Catch.Internal
 import           Effect.Error.Internal
 import           Effect.Reader.Internal
@@ -15,7 +16,7 @@ import           Effect.Sum
 import           Effect.Throw.Internal
 
 class MonadTrans t => AlgebraTrans t where
-  type SigT t :: (* -> *) -> (* -> *)
+  type SigT t :: (Type -> Type) -> (Type -> Type)
 
   algT :: Monad m => SigT t (t m) a -> t m a
 
