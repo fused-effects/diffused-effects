924babf15a8c4a7b4e2257f599944580f03b293d
Rob Rix <rob.rix@me.com>
2020-03-11 18:26:05 -0400

    Copy in send.

--- src/Algebra/GADT.hs
+++ src/Algebra/GADT.hs
@@ -2,11 +2,19 @@
 {-# LANGUAGE TypeFamilies #-}
 module Algebra.GADT
 ( Algebra(..)
+, send
 ) where
 
+import Data.Functor.Identity
 import Data.Kind (Type)
+import Effect.Sum
 
 class Monad m => Algebra m where
   type Sig m :: (Type -> Type) -> (Type -> Type)
 
   alg :: Functor ctx => (forall x . ctx (n x) -> m (ctx x)) -> ctx () -> Sig m n a -> m (ctx a)
+
+
+send :: (Member eff sig, sig ~ Sig m, Algebra m) => eff m a -> m a
+send = fmap runIdentity . alg (fmap Identity . runIdentity) (Identity ()) . inj
+{-# INLINE send #-}
