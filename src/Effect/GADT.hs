310c45ad1ec03e8c2c88e309fe2e5cf4fcc6d733
Rob Rix <rob.rix@me.com>
2020-03-11 18:32:57 -0400

    Define a NonDet effect.

--- src/Effect/GADT.hs
+++ src/Effect/GADT.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
 module Effect.GADT
 ( Choose(..)
 , Empty(..)
 , Error(..)
+, NonDet
 ) where
 
 import Data.Kind (Type)
+import Effect.Sum
 
 data Choose (m :: Type -> Type) k where
   Choose :: Choose m Bool
@@ -17,3 +20,5 @@ data Empty (m :: Type -> Type) k where
 data Error e m k where
   Throw :: e -> Error e m a
   Catch :: m a -> (e -> m a) -> Error e m a
+
+type NonDet = Empty :+: Choose
