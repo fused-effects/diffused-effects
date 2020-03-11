4ca17da786cd31a2d6253a8479d5af71639b6a35
Rob Rix <rob.rix@me.com>
2020-03-11 18:25:16 -0400

    Copy in the Algebra class.

--- src/Algebra/GADT.hs
+++ src/Algebra/GADT.hs
@@ -1,2 +1,12 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
 module Algebra.GADT
-() where
+( Algebra(..)
+) where
+
+import Data.Kind (Type)
+
+class Monad m => Algebra m where
+  type Sig m :: (Type -> Type) -> (Type -> Type)
+
+  alg :: Functor ctx => (forall x . ctx (n x) -> m (ctx x)) -> ctx () -> Sig m n a -> m (ctx a)
